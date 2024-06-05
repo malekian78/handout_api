9eb8fc962768d5f25f62b14ece0dcf58388d32f9
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-05 14:50:32 +0430

    💊 fixing bug in Dockerfile . django project was created in wrong way .... and manage.py could not copy to docker WORKDIR

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,28 +11,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False),
-    SECRET_KEY=(str, "secretKey")
-)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Take environment variables from .env file
-environ.Env.read_env(Path(__file__).parent.parent / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = "django-insecure-@@=on$p=(heqw(^-g6_kde0%4awpk3ex^j!*jo8=gp=7-**b+s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
