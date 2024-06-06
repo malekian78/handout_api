62deb48b35acf0602a4a321e363d67a07f614824
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-06 09:38:25 +0430

    💊fixing bugs & forgoting add Env.read_env(...) to settings.py

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -21,8 +21,11 @@ env = environ.Env(
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(Path(__file__).parent.parent / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
