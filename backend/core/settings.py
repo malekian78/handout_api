307066832cf776cf1f61824a10d77e1fa5f4f95c
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-10 09:13:25 +0430

    📦using python-decouple package for .env

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,28 +11,28 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
+from decouple import config
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False),
-    SECRET_KEY=(str, "secretKey")
-)
+# env = environ.Env(
+#     # set casting, default value
+#     DEBUG=(bool, False),
+#     SECRET_KEY=(str, "secretKey")
+# )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
-environ.Env.read_env(Path(__file__).parent.parent.parent / ".env")
+# environ.Env.read_env(Path(__file__).parent.parent.parent / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY", default="test")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = config("DEBUG", cast=bool, Default=True)
 
 ALLOWED_HOSTS = []
 
