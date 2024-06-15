bfd154e2088d0cd4a91d157464cd09f04199c04d
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-15 09:07:24 +0330

    Update settings.py
    removing additional code

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,11 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
-# env = environ.Env(
-#     # set casting, default value
-#     DEBUG=(bool, False),
-#     SECRET_KEY=(str, "secretKey")
-# )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
