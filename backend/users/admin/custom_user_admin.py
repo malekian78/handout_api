2dc1d6535d380943c8e96234c18cf3c0a37dbec8
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-17 22:18:17 +0430

    create admin folder

---
+++ backend/users/admin/custom_user_admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    # setting filds of what to showing in admin-dashboard
+    model = User
+    list_display = ("id", "email", "is_superuser", "is_active", "is_staff", "created_date")
+    list_display_links = ("id", "email")
+    list_filter = ("email", "is_superuser", "is_active", "is_staff", "created_date")
+    list_editable = ( "is_active", "is_staff",)
+    list_per_page = 20 # default == 100.
+    readonly_fields = ("is_superuser",)
+    search_fields = ("email",)
+    ordering = ("created_date",)
+    fieldsets = (
+        ('Authentication', {
+            'fields': (
+                "email", "password"
+            ),
+        }),
+        ('Permissions', {
+            'fields': (
+                "is_staff", "is_active","is_superuser"
+            ),
+        }),
+    )
+    # setting fields for adding and save new User by admin-dashboard
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_staff",
+                "is_active",
+            )}
+        ),
+    )
