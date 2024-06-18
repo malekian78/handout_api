965435d79cb727f1712f0248ac1a065eb80fbf26
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-18 20:07:39 +0430

    first try but stuck

--- backend/feedback/models.py
+++ backend/feedback/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+#! i will replace this BaseModel class ...and use...  from handout.models import BaseModel
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
