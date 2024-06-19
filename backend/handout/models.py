48898ed09fd034e0f8d2a282a806e602b47edd71
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-19 13:36:44 +0430

    ✔working on FileFileds and improve it

--- backend/handout/models.py
+++ backend/handout/models.py
@@ -1,32 +1,55 @@
+from datetime import datetime
+import os
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(db_index=True, default=timezone.now)
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     class Meta:
         abstract = True
         order_with_respect_to = "created_at"
 
+def validate_file_extension(value):
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.pdf',]
+    if not ext.lower() in valid_extensions:
+        raise ValidationError(_('Unsupported file extension.'))
+
 class Handout(BaseModel):
     name = models.CharField(verbose_name=_("نام جزوه"), max_length=100)
     slug = models.SlugField(verbose_name=_("لینک دسترسی"), unique=True, auto_created=True, help_text="کاربران از طریق این لینک به صفحه جزوه دسترسی پیدا خواهند کرد")
     description = models.TextField(verbose_name=_("توضیحات"),max_length=500)
-    file_name = models.CharField(verbose_name=_("نام فایل"), max_length=150)
+    file_name = models.CharField(verbose_name=_("نام فایل"), max_length=150, editable=False)
     page_count = models.PositiveIntegerField(verbose_name=_("تعداد صفحات جزوه"))
-    file_size = models.PositiveIntegerField(verbose_name=_("اندازه فایل"))
+    file_size = models.PositiveIntegerField(verbose_name=_("اندازه فایل"), editable=False)
     visit_count = models.PositiveIntegerField(verbose_name=_("تعداد بازدید"), default=0)
     publish_time = models.DateField(verbose_name=_("تاریخ انتشار"))
     author = models.ForeignKey("Author", verbose_name=_("نویسنده"), on_delete=models.DO_NOTHING, null=True, blank=True)
-    file = models.FileField(verbose_name=_("آپلود فایل"), upload_to='uploads/{}/{}'.format(author, file_name))
+    file = models.FileField(verbose_name=_("آپلود فایل"), upload_to='uploads/{}/{}'.format(author, file_name), validators=[validate_file_extension])
     category = models.ManyToManyField("Category", verbose_name=_("دسته بندی"), blank=True)
     tag = models.ManyToManyField("Tag", verbose_name=_("تگ"), blank=True)
     def __str__(self):
         return self.name
     class Meta:
         verbose_name_plural = _("Handout")
+        
+    def save(self, *args, **kwargs):
+        if self.file:
+            self.file_name = self.file.name
+            self.file_size = self.file.size
+            self.file.name = self.get_custom_file_name(self.file.name)
+        super().save(*args, **kwargs)
+        
+    def get_custom_file_name(self, original_filename):
+        # Custom logic to rename the file
+        name, ext = os.path.splitext(original_filename)
+        new_name = f"{self.name}_{datetime.now().strftime('%Y%m%d%H%M%S')}{ext}"
+        return new_name
+    
 
 
 class Category(BaseModel):
