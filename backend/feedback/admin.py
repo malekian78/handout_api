ba8fea2e2536d103b990cfde13ccb867b5ceec9f
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-19 15:45:32 +0430

    first try for admin and some improvment in models 🐞but still need some fixingBug

--- backend/feedback/admin.py
+++ backend/feedback/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Comment, Like
 
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    # setting filds of what to showing in admin-dashboard
+    model = Comment
+    list_display = ("id", "name", "email", "handout", "status_choice", "updated_at", "created_at")
+    list_display_links = ("id", "name", "email", "handout",)
+    list_filter = ("name", "created_at", "updated_at")
+    list_editable = ( "status_choice",)
+    search_fields = ("name", "email",)
+    ordering = ("created_at",)
+    
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    # setting filds of what to showing in admin-dashboard
+    model = Like
+    list_display = ("id", "user", "client_ip", "handout", "updated_at", "created_at")
+    list_display_links = ("id", "user", "client_ip",)
+    list_filter = ("handout", "created_at", "updated_at")
+    search_fields = ("handout",)
+    ordering = ("created_at",)
--- backend/feedback/models.py
+++ backend/feedback/models.py
@@ -20,7 +20,7 @@ class Comment(BaseModel):
         ('draft', _('draft')),
         ('published', _('published')),
     )
-    status = models.CharField(statusChoice=status_choice, verbose_name=_("status"), default="draft")
+    status = models.CharField(choices= status_choice, verbose_name=_("status"), default="draft")
 
     def __str__(self):
         return "{}_{}".format(self.name, self.handout)
@@ -30,40 +30,13 @@ class Comment(BaseModel):
 
 class Like(BaseModel):
     #! FIXME: یادت نره مدل یوزر رو بهش امپورت کنی
-    # client_ip = ?
-    user = models.ForeignKey("user" , verbose_name=_("user"), on_delete=models.CASCADE)
-    handout = models.ForeignKey("handout", verbose_name=_("handout"), on_delete=models.CASCADE)
-    rate = models.PositiveSmallIntegerField(verbose_name=_("rate"), max_length=1, default=1)
+    client_ip = models.CharField(verbose_name=_("client_ip"), max_length=150, unique=True, blank=True, null=True)
+    user = models.ForeignKey("User" , verbose_name=_("user"), on_delete=models.CASCADE, blank=True, null=True)
+    handout = models.ForeignKey("Handout", verbose_name=_("handout"), on_delete=models.CASCADE)
     def __str__(self):
         return "{}_{}".format(self.user, self.rate)
     class Meta:
         verbose_name_plural = _("Comment")
     
-
-
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(db_index=True, default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
-    class Meta:
-        abstract = True
-        order_with_respect_to = "created_at"
-
-class Handout(BaseModel):
-    name = models.CharField(verbose_name=_("نام جزوه"), max_length=100)
-    slug = models.SlugField(verbose_name=_("لینک دسترسی"), unique=True, auto_created=True, help_text="کاربران از طریق این لینک به صفحه جزوه دسترسی پیدا خواهند کرد")
-    description = models.TextField(verbose_name=_("توضیحات"),max_length=500)
-    file_name = models.CharField(verbose_name=_("نام فایل"), max_length=150)
-    page_count = models.PositiveIntegerField(verbose_name=_("تعداد صفحات جزوه"))
-    file_size = models.PositiveIntegerField(verbose_name=_("اندازه فایل"))
-    visit_count = models.PositiveIntegerField(verbose_name=_("تعداد بازدید"), default=0)
-    publish_time = models.DateField(verbose_name=_("تاریخ انتشار"))
-    author = models.ForeignKey("Author", verbose_name=_("نویسنده"), on_delete=models.DO_NOTHING, null=True, blank=True)
-    file = models.FileField(verbose_name=_("آپلود فایل"), upload_to='uploads/{}/{}'.format(author, file_name))
-    category = models.ManyToManyField("Category", verbose_name=_("دسته بندی"), blank=True)
-    tag = models.ManyToManyField("Tag", verbose_name=_("تگ"), blank=True)
-    def __str__(self):
-        return self.name
-    class Meta:
-        verbose_name_plural = _("Handout")
-
+    # FIXME: اگر نه کاربر و نه همچنین کلاینتی وجود داشت باید حتما اون فیلد ذخیره نشه
+    
