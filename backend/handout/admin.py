f3c8df81191f59ca1f94fd2cc480ac657f6b94a9
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-22 09:55:27 +0430

    removing model from admin.py for handout app

--- backend/handout/admin.py
+++ backend/handout/admin.py
@@ -6,7 +6,6 @@ from django.utils.translation import gettext_lazy as _
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     # setting filds of what to showing in admin-dashboard
-    model = Author
     list_display = ("id", "name", "updated_at", "created_at")
     list_display_links = ("id", "name")
     list_filter = ("name", "created_at", "updated_at")
@@ -15,7 +14,6 @@ class AuthorAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     # setting filds of what to showing in admin-dashboard
-    model = Category
     list_display = ("id", "name", "parent", "updated_at", "created_at")
     list_display_links = ("id", "name", "parent")
     list_filter = ("name", "parent", "created_at", "updated_at")
@@ -33,7 +31,6 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Handout)
 class HandoutAdmin(admin.ModelAdmin):
     # setting filds of what to showing in admin-dashboard
-    model = Handout
     list_display = ("id", "name", "publish_time", "author", "visit_count", "updated_at", "created_at")
     prepopulated_fields = {'slug':('name',)}
     list_display_links = ("id", "name", "author",)
@@ -46,7 +43,6 @@ class HandoutAdmin(admin.ModelAdmin):
 @admin.register(Tag)
 class TagrAdmin(admin.ModelAdmin):
     # setting filds of what to showing in admin-dashboard
-    model = Tag
     list_display = ("id", "name", "updated_at", "created_at")
     list_display_links = ("id", "name")
     list_filter = ("name", "created_at", "updated_at")
