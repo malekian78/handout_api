1bc47c898e408c739058e93345df509b0c91dcce
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-24 10:47:33 +0430

    🤔 add related_name='maps' to user

--- backend/feedback/models.py
+++ backend/feedback/models.py
@@ -30,7 +30,7 @@ class Comment(BaseModel):
 
 class Like(BaseModel):
     client_ip = models.GenericIPAddressField(verbose_name=_("client_ip"), max_length=150)
-    user = models.ForeignKey(User , verbose_name=_("user"), on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User , related_name='maps', verbose_name=_("user"), on_delete=models.CASCADE, blank=True, null=True)
     handout = models.ForeignKey(Handout, verbose_name=_("handout"), on_delete=models.CASCADE)
     class Meta:
         verbose_name = _("Like")
