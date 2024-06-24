2d3bbc7d12220206365bf228d3b91ad16fea3020
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-24 10:46:54 +0430

    changing the models.py

--- backend/feedback/models.py
+++ backend/feedback/models.py
@@ -1,10 +1,8 @@
 from users.models import User
-from handout.models import BaseModel, Handout
+from handout.models import Handout
+from utils.base_model import BaseModel
 from django.db import models
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-# from ..handout.models import BaseModel, Handout
-
 
 
 class Comment(BaseModel):
@@ -12,32 +10,32 @@ class Comment(BaseModel):
     name = models.CharField(verbose_name=_("name"), max_length=150)
     email = models.EmailField(verbose_name=_("email"), max_length=150)
     body = models.TextField(verbose_name=_("Message"))
+    draft = 0
+    published = 1
+    rejected = 2
     status_choice = (
-        ('draft', _('draft')),
-        ('published', _('published')),
+        (0, _('draft')),
+        (1, _('published')),
+        (2, _('rejected')),
     )
-    status = models.CharField(choices= status_choice, verbose_name=_("status"), default="draft")
-
-    def __str__(self):
-        return "{}_{}".format(self.name, self.handout)
+    status = models.PositiveSmallIntegerField(choices= status_choice, verbose_name=_("status"), default=0)
     class Meta:
-        verbose_name_plural = _("Comment")
+            verbose_name = _("Comment")
+            verbose_name_plural = _("Comments")
+            
+    def __str__(self):
+        return f"{self.name}_{self.handout}"
+    
 
 
 class Like(BaseModel):
-    client_ip = models.CharField(verbose_name=_("client_ip"), max_length=150, unique=True, blank=True, null=True)
+    client_ip = models.GenericIPAddressField(verbose_name=_("client_ip"), max_length=150)
     user = models.ForeignKey(User , verbose_name=_("user"), on_delete=models.CASCADE, blank=True, null=True)
     handout = models.ForeignKey(Handout, verbose_name=_("handout"), on_delete=models.CASCADE)
-    def __str__(self):
-        return "{}_{}".format(self.handout, self.user)
     class Meta:
-        verbose_name_plural = _("Like")
-
-    def save(self, *args, **kwargs):
-        # prevent a category to be itself parent
-        if self.client_ip is None and self.user is None:
-            # self.unique_error_message("test")
-            return
-            # raise ValueError(_("client_ip and user could not be null for both"))
-        super().save(*args, **kwargs)
+        verbose_name = _("Like")
+        verbose_name_plural = _("Likes")
+    
+    def __str__(self):
+        return f"{self.handout}_{self.user}"
     
