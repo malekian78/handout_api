104a405134bb08472381bc9e518b4d788476e5e8
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-24 09:56:07 +0430

    change admin.py to admin folder

---
+++ backend/handout/admin/__init__.py
@@ -0,0 +1,4 @@
+from .author_admin import AuthorAdmin
+from .category_admin import CategoryAdmin
+from .handout_admin import HandoutAdmin
+from .tag_admin import TagAdmin
---
+++ backend/handout/admin/author_admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from handout.models import Author
+from django.utils.translation import gettext_lazy as _
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "updated_at", "created_at")
+    list_display_links = ("id", "name")
+    list_filter = ("name", "created_at", "updated_at")
+    search_fields = ("name",)
+    ordering = ("created_at",)
