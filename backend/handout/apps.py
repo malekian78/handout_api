4b5e8b3913ff158383e6696b3f6cab4c7d08894b
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-26 14:38:34 +0430

    don't need Meta class ... for handout and user

--- backend/handout/apps.py
+++ backend/handout/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class HandoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'handout'
+    verbose_name = _("handout Config")
+    verbose_name_plural = _("handout Configs")
