51cba616745608ada3f4e91fac60ae754ce1d370
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-26 14:37:03 +0430

    don't need Meta class ...

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -5,7 +5,6 @@ from django.utils.translation import gettext_lazy as _
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
-    
-    class Meta:
-        verbose_name = _("User Config")
-        verbose_name_plural = _("User Configs")
+    verbose_name = _("User Config")
+    verbose_name_plural = _("User Configs")
+
