64127e749ef0bcbd3f0cd9b556eeacfadff280d6
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-06-26 14:40:27 +0430

    don't need Meta class... for feedback

--- backend/feedback/apps.py
+++ backend/feedback/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class FeedbackConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'feedback'
+    verbose_name = _("Feedback Config")
+    verbose_name_plural = _("Feedback Configs")
