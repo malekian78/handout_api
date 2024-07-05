cf3dfd6d73b74e7bb67cf389fb2e0a2d448076e6
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-07-05 17:08:19 +0430

    ✔using config('DJANGO_SETTINGS_MODULE')

--- backend/core/settings/__init__.py
+++ backend/core/settings/__init__.py
@@ -1,10 +1,9 @@
+import os
 from decouple import config
 
-ENVIRONMENT = config('DJANGO_ENV', default='production')
+settings_module = config('DJANGO_SETTINGS_MODULE', default="core.settings.test")
 
-if ENVIRONMENT == 'production':
-    from .production import *
-elif ENVIRONMENT == 'test':
-    from .test import *
-else:
-    from .local import *
+if not settings_module:
+    raise ValueError("The DJANGO_SETTINGS_MODULE environment variable is not set")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
