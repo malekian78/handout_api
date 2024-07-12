66d3035f83796b8025661e6e6c620e4033be5620
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-07-12 09:53:38 +0430

    changing test for category to check each field in the output is exist.

--- backend/handout/tests/test_category_api.py
+++ backend/handout/tests/test_category_api.py
@@ -2,10 +2,15 @@ import pytest
 from django.urls import reverse
 
 @pytest.mark.django_db
-def test_get_category_response_200_status(client):
+def test_get_category_response_200_status(client, category):
     url = reverse('handout:category-list')
     response = client.get(url)
-    assert response.status_code == 200
+    print("url:",url)
+    actual_data = response.json()
+    expected_fields = {'id', 'name', 'slug', 'parent', 'children'}
+    print("actual_data:",actual_data)
+    for item in actual_data:
+        assert expected_fields.issubset(item.keys()), f"Missing fields in item: {item}"
 
 @pytest.mark.django_db
 def test_get_category_detail_response_200_status(client, category):
