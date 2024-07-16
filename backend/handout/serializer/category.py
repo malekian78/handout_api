970349a0b07cad24ca062c31b8c8d5fb3d56972f
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-07-16 14:08:43 +0430

    reformat using category serializer for custom eschema

--- backend/handout/serializer/category.py
+++ backend/handout/serializer/category.py
@@ -1,14 +1,36 @@
+from drf_spectacular.utils import OpenApiExample, extend_schema_serializer
 from rest_framework import serializers
 
 from handout.models import Category
 
 
+@extend_schema_serializer(
+    examples=[OpenApiExample("Category Detail Example", value={"id": 1, "name": "Electronics", "slug": "electronics"})]
+)
 class CategoryDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ["id", "name", "slug"]
 
 
+@extend_schema_serializer(
+    examples=[
+        OpenApiExample(
+            "Category Example",
+            value={"id": 1, "name": "Electronics", "slug": "electronics", "parent": None, "children": []},
+        ),
+        OpenApiExample(
+            "Category with Children Example",
+            value={
+                "id": 2,
+                "name": "Computers",
+                "slug": "computers",
+                "parent": {"id": 1, "name": "Electronics", "slug": "electronics"},
+                "children": [],
+            },
+        ),
+    ]
+)
 class CategorySerializer(serializers.ModelSerializer):
     parent = CategoryDetailSerializer(read_only=True)
     children = CategoryDetailSerializer(many=True, read_only=True)
--- backend/handout/views/category.py
+++ backend/handout/views/category.py
@@ -2,49 +2,8 @@ from rest_framework import viewsets
 
 from handout.models import Category
 from handout.serializer import CategorySerializer
-from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
 
 
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
-    serializer_class = CategorySerialize
-    @extend_schema(
-        operation_id='DetailCategory',
-        tags=['Category'],
-        summary='Detail of a category',
-        description='Returns the details of a specific category base on id.',
-        responses={
-            200: CategorySerializer,
-            404: OpenApiResponse(description='Category not found'),
-        },
-        examples=[
-            OpenApiExample(
-                'Category Detail Example',
-                value={'id': 1, 'name': 'Electronics', 'slug': 'electronics', 'parent': None, 'children': []}
-            )
-        ],
-    )
-    def retrieve(self, request, *args, **kwargs):
-        return super().retrieve(request, *args, **kwargs)
-
-    @extend_schema(
-        operation_id='listCategories',
-        tags=['Category'],
-        summary='Retrieve a list of categories',
-        description='Returns a list of all categories in the system.',
-        responses={
-            200: CategorySerializer(many=True),
-        },
-        examples=[
-            OpenApiExample(
-                'Category List Example',
-                value=[
-                    {'id': 1, 'name': 'Electronics', 'slug': 'electronics', 'parent': None, 'children': []},
-                    {'id': 2, 'name': 'Books', 'slug': 'books', 'parent': None, 'children': []}
-                ]
-            )
-        ],
-    )
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-
+    serializer_class = CategorySerializer
