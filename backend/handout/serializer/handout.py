d4a200efe191fdc30bad9cff727839c84e264d43
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-07-16 14:26:35 +0430

    ✔adding custome eschema example for handout api

--- backend/handout/serializer/handout.py
+++ backend/handout/serializer/handout.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import OpenApiExample, extend_schema_serializer
 from rest_framework import serializers
 
 from handout.models import Handout, Tag
@@ -10,6 +11,41 @@ class TagSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "slug"]
 
 
+@extend_schema_serializer(
+    examples=[
+        OpenApiExample(
+            "Handout List Example",
+            value={
+                "id": 1,
+                "name": "mamad1",
+                "slug": "mamad",
+                "visit_count": 0,
+                "publish_time": "1403-03-30",
+                "author": "ali",
+                "category": [{"id": 1, "name": "دیجیتالی", "slug": "digital"}],
+                "tag": [{"id": 1, "name": "tag1", "slug": "tag1"}, {"id": 2, "name": "tag2", "slug": "tag2"}],
+            },
+        ),
+        OpenApiExample(
+            "Handout Detail Example",
+            value={
+                "id": 1,
+                "name": "mamad1",
+                "description": "test description",
+                "slug": "mamad",
+                "page_count": 5,
+                "visit_count": 0,
+                "publish_time": "1403-03-30",
+                "author": "ali",
+                "file_size": 113120,
+                "file_name": "agahi2",
+                "file": "http://127.0.0.1:8000/media/handouts/ali/agahi2.pdf",
+                "category": [{"id": 1, "name": "دیجیتالی", "slug": "digital"}],
+                "tag": [{"id": 1, "name": "tag1", "slug": "tag1"}, {"id": 2, "name": "tag2", "slug": "tag2"}],
+            },
+        ),
+    ]
+)
 class HandoutSerializer(serializers.ModelSerializer):
     category = CategoryDetailSerializer(many=True)
     tag = TagSerializer(many=True)
