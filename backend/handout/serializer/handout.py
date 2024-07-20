b08c93eaee71de521694d0f6982a86804105ab96
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-07-20 20:36:37 +0430

    ✔reformat - example of document serializer

--- backend/handout/serializer/handout.py
+++ backend/handout/serializer/handout.py
@@ -31,25 +31,7 @@ class AuthorSrializer(serializers.ModelSerializer):
                 "category": [{"id": 1, "name": "دیجیتالی", "slug": "digital"}],
                 "tag": [{"id": 1, "name": "tag1", "slug": "tag1"}, {"id": 2, "name": "tag2", "slug": "tag2"}],
             },
-        ),
-        OpenApiExample(
-            "Handout Detail Example",
-            value={
-                "id": 1,
-                "name": "mamad1",
-                "description": "test description",
-                "slug": "mamad",
-                "page_count": 5,
-                "visit_count": 0,
-                "publish_time": "1403-03-30",
-                "author": "ali",
-                "file_size": 113120,
-                "file_name": "agahi2",
-                "file": "http://127.0.0.1:8000/media/handouts/ali/agahi2.pdf",
-                "category": [{"id": 1, "name": "دیجیتالی", "slug": "digital"}],
-                "tag": [{"id": 1, "name": "tag1", "slug": "tag1"}, {"id": 2, "name": "tag2", "slug": "tag2"}],
-            },
-        ),
+        )
     ]
 )
 class HandoutListSerializer(serializers.ModelSerializer):
@@ -71,6 +53,28 @@ class HandoutListSerializer(serializers.ModelSerializer):
         ]
 
 
+@extend_schema_serializer(
+    examples=[
+        OpenApiExample(
+            "Handout Detail Example",
+            value={
+                "id": 1,
+                "name": "mamad1",
+                "description": "test description",
+                "slug": "mamad",
+                "page_count": 5,
+                "visit_count": 0,
+                "publish_time": "1403-03-30",
+                "author": "ali",
+                "file_size": 113120,
+                "file_name": "agahi2",
+                "file": "http://127.0.0.1:8000/media/handouts/ali/agahi2.pdf",
+                "category": [{"id": 1, "name": "دیجیتالی", "slug": "digital"}],
+                "tag": [{"id": 1, "name": "tag1", "slug": "tag1"}, {"id": 2, "name": "tag2", "slug": "tag2"}],
+            },
+        ),
+    ]
+)
 class HandoutDetailSerializer(serializers.ModelSerializer):
     category = CategoryDetailSerializer(many=True)
     tag = TagSerializer(many=True)
